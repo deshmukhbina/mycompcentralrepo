edd468e6e6db31d27a8759bdea75dcc7b16c38e7
RAJVEER <RAJVEER@RAJVEER-PC>
2017-05-18 11:57:07 +0530

    change in AgeComparator.java constructor

--- CollectionFramework/src/sorting/AgeComparator.java
+++ CollectionFramework/src/sorting/AgeComparator.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 public class AgeComparator implements Comparator<Student> {
 
 	public int compare(Student o1, Student o2) {
+		System.out.println("  ");
 		return o1.getAge()-o2.getAge();
 	}
     
